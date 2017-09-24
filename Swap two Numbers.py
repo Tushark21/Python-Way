b243857a5d7d898d8715b2d93f56c055eb96c776
Tushark21 <tushargautamk21@gmail.com>
2017-09-24 23:41:51 +0530

    Program to swap two Numbers

---
+++ Swap two Numbers.py
@@ -0,0 +1,11 @@
+x=int(input("Enter 1st Number:"))
+y=int(input("Enter 2nd Number:"))
+
+print("Numbers Before Swapping:")
+print(x,",",y)
+
+temp=x
+x=y
+y=temp
+print("Numbers After Swapping:")
+print(x,",",y)
