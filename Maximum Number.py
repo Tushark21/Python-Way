ea7c9baa9202ed071b421a195d00a9514d8d822a
Tushark21 <tushargautamk21@gmail.com>
2017-09-26 23:08:29 +0530

    Program to find Maximum Number from three Numbers using if-else

---
+++ Maximum Number.py
@@ -0,0 +1,13 @@
+a=int(input("Enter 1st Number:"))
+b=int(input("Enter 2nd Number:"))
+c=int(input("Enter 3rd Number:"))
+
+max=a;
+
+if b>max:
+    max=b;
+
+if c>max:
+    max=c;
+
+print(max,"is Max");
